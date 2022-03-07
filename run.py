d6a96973667e9a081fc399832e1ab6952abf6af8
eirikstrompedersen <eirikstrompedersen@gmail.com>
2022-03-07 19:15:31 +0000

    Add basic project structure, and test basic code in terminal

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+hei = "Hello there little friend"
+
+def printstatment():
+    print(hei)
+
+printstatment()
+
