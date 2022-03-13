b5391e2b35e55cc78d19a64c7478ba5c2c819254
eirikstrompedersen <eirikstrompedersen@gmail.com>
2022-03-13 20:58:57 +0000

    Add start command

--- run.py
+++ run.py
@@ -17,50 +17,46 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('measurements')
 
 raw_obs = SHEET.worksheet("observation")
+valid = SHEET.worksheet("obs_valid")
 
-
-def file_content():
+def file_attributes():
     """
     Give the user a insight of the attribute fields in the speadsheet
     """
+    print("The spreadsheet containt theese attribute fields:\n")
     file_attributes = raw_obs.row_values(1)
-    print("The spreadsheet containt theese attribute fields:")
     print(file_attributes)
 
-
-def remove_columns():
+def start():
     """
-    Removes columns from spreadsheet
-    """
-    columns = []
-    
-
-
-
-#def field_validator():
-    """
-    Validate if any datafield does not contain data
+    User has to input "Start" to start the operation
     """
     
-    if 
+    try:
+        execute = str(input("\nType 'Start' here: "))
+        print("Process is starting...")
+    except ValueError:
+        print("Invalid command")
 
 
-#def emtpy_fields():
+def copy_worksheet():
     """
-    Give empty data fields the "None" attribute.
+    Copy the data from the observation worksheet to the obs_valid worksheet.
+    Reasoning for doing this is to keep the raw measurments intact.
     """
-    
-
-
-
+    print("\nCopying raw data to new worksheet...")
+    get_all = raw_obs.get_all_values()
+    get_all.append("valid")
+    print("Data is successfully copied!\n")
 
 
 def main():
     """
     Run the program functions
     """
-    file_content()
-    print(raw_obs.get_all_records())
+    file_attributes()
+    start()
+    copy_worksheet()
 
 
 
