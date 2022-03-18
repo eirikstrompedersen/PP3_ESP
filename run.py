9e9ec1950b4c7bd9799b60b22764944bcdd7dbb9
eirikstrompedersen <eirikstrompedersen@gmail.com>
2022-03-18 23:42:52 +0000

    Tidy up code

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-To solve this assigment, documentation from gspread.org is used
+To solve this assigment, documentation from gspread.org is used.
 """
 
 import gspread
@@ -19,6 +19,7 @@ SHEET = GSPREAD_CLIENT.open('measurements')
 
 raw_obs = SHEET.worksheet("observation")
 
+
 def file_attributes():
     """
     Give the user a insight of the attribute fields in the speadsheet
@@ -28,23 +29,25 @@ def file_attributes():
     file_attri = raw_obs.row_values(1)
     print(file_attri)
 
+
 def start():
     """
     Ask user to type a command to start the program.
     """
     while True:
-        user_execute = str(input("\nPlease type 'Start' to run the program \n")) 
+        user_execute = str(input("\nPlease type 'Start' to run the program \n"))
         """
         Validates the user input. If input does not match requierment,
         printstatement launches.
         """
-        if not user_execute == "Start":                 
+        if not user_execute == "Start":
             print("Invalid input, please try again")
         else:
             """
-            If comamnd is correct (aka 'Start'), program is allowed to contuine running.
-            """                                               
-            print("Program is starting up.....")
+            If comamnd is correct (aka 'Start'),
+            program is allowed to contuine running.
+            """
+            print("\nProgram is starting up.....")
             break
 
 
@@ -54,10 +57,12 @@ def dublicate_worksheet():
     Dublicates 'observations" into a new worksheet called obs_copy.
     Reasoning for doing this is to keep the raw measurments intact.
     """
-    print("Dublicate 'observation' in the spreadsheet...")
+    print("\nDublicate 'observation' in the spreadsheet...")
     """
-    Dublicates the "observation" worksheet, and gives the dublication
-    the name "obs_copy". Indexing the new worksheet to index 1 in the spreadsheet.
+    Dublicates the "observation" worksheet,
+    and gives the dublication
+    the name "obs_copy". Indexing the
+    new worksheet to index 1 in the spreadsheet.
     """
     raw_obs.duplicate(insert_sheet_index=1, new_sheet_name="obs_copy")
     print("Worksheet 'obs_copy' is created")
@@ -68,14 +73,16 @@ def delete_columns():
     """
     This function removes unwatned columns form the worksheet
     """
+    print("\nFile is being cleaned, remain patient...")
     obs_copy.delete_columns(5, 6)
     obs_copy.delete_columns(9, 17)
     obs_copy.delete_columns(9, 15)
+    obs_copy.delete_rows(164, 1000)
 
     file_attri = obs_copy.row_values(1)
-    print("\nFile is now processed, here is the remaining file attributes: \n")
+    print("File is now processed, here is the remaining file attributes:")
     print(file_attri)
-    
+
 
 def main():
     """
@@ -84,7 +91,7 @@ def main():
     file_attributes()                # Give the user insight in the file attributes in the spreadsheet.
     start()                          # Ask user to start the program with a defined command.
     dublicate_worksheet()            # Dublicates the worksheet, to save the raw measuerments.
-    delete_columns()                 # Remove unwanted columns in worksheet
+    delete_columns()                 # Remove unwanted columns in worksheet 
 
 
 print("-------------------------------------------------------")
